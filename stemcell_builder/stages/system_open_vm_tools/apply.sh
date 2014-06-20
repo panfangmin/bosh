9f9040aaef94f925265cca339bf0b9921dfa8bfc
Adam Stegman <astegman@pivotallabs.com>
2014-06-20 15:01:01 -0700

    wip
    typo

--- stemcell_builder/stages/system_open_vm_tools/apply.sh
+++ stemcell_builder/stages/system_open_vm_tools/apply.sh
@@ -6,7 +6,7 @@ base_dir=$(readlink -nf $(dirname $0)/../..)
 source $base_dir/lib/prelude_apply.bash
 source $base_dir/lib/prelude_bosh.bash
 
-if [ `os_type` == "ubuntu" ]
+if [ `os_type` == "ubuntu" ]; then
   pkg_mgr install open-vm-tools
 else
   echo "No installation strategy for open-vm-tools, exiting..."
